d72053a1f7780b34f2054befe4452e90955916f6
Ly Nate <njl36@zoo.cs.yale.edu>
2025-01-29 10:34:09 -0500

    submitted

--- python/test_cribbage.py
+++ python/test_cribbage.py
@@ -10,7 +10,7 @@ from my_policy import MyPolicy
 if __name__ == "__main__":
     start_time = time.time()
 
-    games = 100000
+    games = 10000
     run_time = 0
     if len(sys.argv) > 1:
         if sys.argv[1] == "--time":
